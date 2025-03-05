fbd20ed4656ac11571d5fd24bffc972dbb706b9c
olvera-paulino-arturo <olvera.paulino.arturo@gmail.com>
2025-03-04 19:24:05 -0600

    Add FirebaseLoader

---
+++ bookmarks/SupportingFiles/Managers/Loaders/FirebaseLoader.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseLoader.swift
+//  bookmarks
+//
+//  Created by Arturo Olvera Paulino on 03/03/25.
+//
+
+
+import FirebaseCore
+
+struct FirebaseLoader {
+    static func loader() {
+        FirebaseApp.configure()
+    }
+    
+    
+}
